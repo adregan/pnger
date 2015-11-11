df40d342bc1f7bc3b08d077dd4beaf1bdf580fa8
Duncan Regan <duncanregan@gmail.com>
2015-11-11 16:15:04 -0500

    directly assigning

--- filter.py
+++ filter.py
@@ -3,31 +3,28 @@ from filters import Filters
 filt = Filters('filter')
 
 def filterer(scanlines, bpp):
-    scanline_bytes = [list(scanline.get('bytes')) for scanline in scanlines]
+    filtered = [list(scanline.get('bytes')) for scanline in scanlines]
 
     def filter_byte(filter_type, current_byte, y, x):
         a = b = c = 0
         try:
-            a = scanline_bytes[y][x - bpp]
+            a = filtered[y][x - bpp]
         except IndexError as err:
             pass
         try:
-            b = scanline_bytes[y - 1][x]
+            b = filtered[y - 1][x]
         except IndexError as err:
             pass
         try:
-            c = scanline_bytes[y - 1][x - bpp]
+            c = filtered[y - 1][x - bpp]
         except IndexError as err:
             pass
 
         return filt[filter_type](current_byte, a, b, c)
 
-    filtered = []
-
     for y, scanline in enumerate(scanlines):
-        filtered_bytes = [
-            filter_byte(scanline.get('type'), current_byte, y, x)
-            for x, current_byte in enumerate(scanline.get('bytes'))]
-
+        for x, current_byte in enumerate(scanline.get('bytes')):
+            scanline_bytes[y][x] = filter_byte(
+                scanline.get('type'), current_byte, y, x)
 
     return filtered
--- reconstruct.py
+++ reconstruct.py
@@ -1,7 +1,9 @@
 from filters import Filters
 
+filt = Filters('reconstruct')
+
 def reconstructer(scanlines, bpp):
-    reconstructed = []
+    reconstructed = [list(scanline.get('bytes')) for scanline in scanlines]
 
     def reconstruct_byte(filter_type, current_byte, y, x):
         a = b = c = 0
@@ -18,13 +20,11 @@ def reconstructer(scanlines, bpp):
         except IndexError as err:
             pass
 
-        return Filters[filter_type](current_byte, a, b, c)
+        return filt[filter_type](current_byte, a, b, c)
 
     for y, scanline in enumerate(scanlines):
-        reconstructed_bytes = [
-            reconstruct_byte(scanline.get('type'), current_byte, y, x)
-            for x, current_byte in enumerate(scanline.get('bytes'))]
-
-        reconstructed.append(reconstructed_bytes)
+        for x, current_byte in enumerate(scanline.get('bytes')):
+            reconstructed[y][x] = reconstruct_byte(
+                scanline.get('type'), current_byte, y, x)
 
     return reconstructed
