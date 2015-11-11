31ca04ddbfd61b153cbad63cd43c88aa645772e1
Duncan Regan <duncanregan@gmail.com>
2015-11-11 18:36:35 -0500

    correcting naming

--- filter.py
+++ filter.py
@@ -3,20 +3,21 @@ from filters import Filters
 filt = Filters('filter')
 
 def filterer(scanlines, bpp):
-    filtered = [list(scanline.get('bytes')) for scanline in scanlines]
+    original = [list(scanline.get('bytes')) for scanline in scanlines]
+    filtered = list(original)
 
     def filter_byte(filter_type, current_byte, y, x):
         above = y - 1
         left = x - bpp
-        a = 0 if left < 0 else filtered[y][left]
-        b = 0 if above < 0 else filtered[above][x]
-        c = 0 if (above < 0 or left < 0) else filtered[above][left]
+        a = 0 if left < 0 else original[y][left]
+        b = 0 if above < 0 else original[above][x]
+        c = 0 if (above < 0 or left < 0) else original[above][left]
 
         return filt[filter_type](current_byte, a, b, c)
 
     for y, scanline in enumerate(scanlines):
         for x, current_byte in enumerate(scanline.get('bytes')):
-            scanline_bytes[y][x] = filter_byte(
+            filtered[y][x] = filter_byte(
                 scanline.get('type'), current_byte, y, x)
 
     return filtered
