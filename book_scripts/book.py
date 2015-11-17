85abe941a54b5f79ddd0683b872197b4b2c3a4fe
Duncan Regan <duncanregan@gmail.com>
2015-11-17 13:14:38 -0500

    moves the book scripts to a book_scripts folder

--- book_scripts/book.py
+++ book_scripts/book.py
@@ -1,17 +1,21 @@
 import json
 
-def make_book(file_name):
+def make_book(file_name, book_title, author):
     with open('{}.json'.format(file_name), 'r') as file:
         data = json.loads(file.read())
 
     num_of_chapters = len(data)
 
     with open('book.txt', 'w') as file:
-        file.write('The Cover of The Sun Also Rises\n\n\n')
-        file.write('By Duncan Regan\n\n\n')
+        file.write('{}\n\n\n'.format(book_title))
+        file.write('By {}\n\n\n'.format(author))
         for i, chapter in enumerate(data):
             file.write('Chapter {}\n\n'.format(i+1))
             file.write('{}\n\n'.format('. '.join(chapter)))
 
 if __name__ == '__main__':
-    make_book('cover_colors')
+    make_book(
+        'cover_colors',
+        book_title='The Cover of The Sun Also Rises',
+        author='Duncan Regan'
+    )
