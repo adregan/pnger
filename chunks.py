6dae5b5932f0a54454890fe9d5c50d1bf916cddb
Duncan Regan <duncanregan@gmail.com>
2015-11-19 16:28:56 -0500

    creating ihdr data and chunks

--- chunks.py
+++ chunks.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 import zlib
+import struct
+from errors import InvalidPNG
 
 def split_into_chunks(file_bytes, chunks=[]):
     ''' chunks should look like this:
@@ -42,6 +44,43 @@ def parse_ihdr_data(ihdr_chunk):
         filter_type = ihdr_data[11],
         interlace_type = ihdr_data[12]
     )
+
+def create_chunk(data, type):
+    chunk_type = bytes(type, 'utf-8')
+    chunk_length = bytes(str(len(data)), 'utf-8')
+    # TODO: CRC FOR REAL http://www.w3.org/TR/PNG/#5CRC-algorithm
+    crc = bytes(4)
+
+    return chunk_length + chunk_type + chunk_data + crc
+
+def create_ihdr_data(
+    width,
+    height,
+    bit_depth=8,
+    color_type=2, 
+    compression_type=0,
+    interlace_type=0):
+
+    # Filter type is ALWAYS 0
+    filter_type=0
+
+    if bit_depth != 8:
+        raise NotImplementedError(
+            'Sorry, bit depths other than 8 have not yet been implemented')
+
+    width_b = struct.pack('<I', width)
+    height_b = struct.pack('<I', height)
+    bit_depth_b = bytes(str(bit_depth), 'utf-8')
+    color_type_b = bytes(str(color_type), 'utf-8')
+    compression_type_b = bytes(str(compression_type), 'utf-8')
+    filter_type_b = bytes(str(filter_type), 'utf-8')
+    interlace_type_b = bytes(str(interlace_type), 'utf-8')
+
+    data = (
+        width_b + height_b + bit_depth_b + color_type_b + 
+        compression_type_b + filter_type_b + interlace_type_b)
+
+    return data
 
 def parse_header_and_data(chunks):
     ''' Returns the IHDR and IDAT from the chunks
